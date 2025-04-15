eae539d5e5a63dc6f00971d313f24595643ddedd
Ferrusia <ziminior18@gmail.com>
2025-04-15 21:39:15 +0700

    Add files via upload

---
+++ jawajawa.js
@@ -0,0 +1,8 @@
+const sumary = document.getElementById("Zussamenfassung");
+sumary.addEventListener("click", function(){
+    document.getElementById("sumtext").textContent = "Kesimpulan Leonhard Euler adalah seorang matematikawan Swiss yang memberikan kontribusi besar pada berbagai bidang matematika dan fisika termasuk geometri analitik, trigonometri, geometri, kalkulus, dan teori bilangan.";
+});
+const awalan = document.getElementById("awalan");
+awalan.addEventListener("click", function(){
+    document.getElementById("sumtext").textContent = "Ayah dari Leonhard Euler adalah Paul Euler. Beliau telah belajar teologi di Universitas Basel dan menghadiri kuliah Jacob Bernoulli di sana. Faktanya, Paul Euler dan Johann Bernoulli pernah tinggal di rumah Jacob Bernoulli saat kuliah di Basel. Paul Euler menjadi pendeta Protestan dan menikahi Margaret Brucker, putri pendeta Protestan lainnya. Putra mereka Leonhard Euler lahir di Basel, tetapi keluarganya pindah ke Riehen saat ia berusia satu tahun dan di Riehen, tidak jauh dari Basel, Leonard dibesarkan. Paul Euler memiliki, seperti yang telah kami sebutkan, beberapa pelatihan matematika dan ia mampu mengajarkan matematika dasar kepada putranya beserta mata pelajaran lainnya.";
+});
